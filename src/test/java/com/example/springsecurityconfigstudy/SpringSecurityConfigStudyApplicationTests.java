171143bb11ac0d4ad5ec0e9c7b8277c824332dc6
HyeongJu Yoo <zerofunc@naver.com>
2022-09-22 00:09:31 +0900

    init

---
+++ src/test/java/com/example/springsecurityconfigstudy/SpringSecurityConfigStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityconfigstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityConfigStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
