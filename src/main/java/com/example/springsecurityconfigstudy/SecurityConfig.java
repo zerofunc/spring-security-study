94938f03232f35e316315a43cce9d5b107f891f7
HyeongJu Yoo <zerofunc@naver.com>
2022-09-25 17:22:23 +0900

    권한 처리

--- src/main/java/com/example/springsecurityconfigstudy/SecurityConfig.java
+++ src/main/java/com/example/springsecurityconfigstudy/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.springsecurityconfigstudy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
@@ -26,15 +27,25 @@ import java.io.IOException;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user").password("{noop}1111").roles("USER");
+
+        auth.inMemoryAuthentication().withUser("sys").password("{noop}1111").roles("SYS", "USER");
+
+        auth.inMemoryAuthentication().withUser("admin").password("{noop}1111").roles("ADMIN", "SYS", "USER");
+    }
+
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers("/user").hasRole("USER")
+                .antMatchers("/admin/**").access("hasRole('ADMIN') or hasRole('SYS')")
+                .antMatchers("/admin/pay").hasRole("ADMIN")
                 .anyRequest().authenticated();
-        http.
-                formLogin();
         http
-                .sessionManagement()
-                .sessionFixation().changeSessionId();
+                .formLogin();
 
     }
 }
--- src/main/java/com/example/springsecurityconfigstudy/SecurityController.java
+++ src/main/java/com/example/springsecurityconfigstudy/SecurityController.java
@@ -11,6 +11,21 @@ public class SecurityController {
         return "home";
     }
 
+    @GetMapping("/user")
+    public String user() {
+        return "user";
+    }
+
+    @GetMapping("/admin/pay")
+    public String adminPay() {
+        return "adminPay";
+    }
+
+    @GetMapping("/admin/**")
+    public String admin() {
+        return "admin";
+    }
+
     @GetMapping("loginPage")
     public String loginPage() {
         return "loginPage";
