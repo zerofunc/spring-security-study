da304aba357958430c3d35998082b51069ca8f68
HyeongJu Yoo <zerofunc@naver.com>
2022-09-25 17:57:40 +0900

    exceptionhadling

--- src/main/java/com/example/springsecurityconfigstudy/SecurityController.java
+++ src/main/java/com/example/springsecurityconfigstudy/SecurityController.java
@@ -26,6 +26,16 @@ public class SecurityController {
         return "admin";
     }
 
+    @GetMapping("/denied")
+    public String denied() {
+        return "Access is denied";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
     @GetMapping("loginPage")
     public String loginPage() {
         return "loginPage";
