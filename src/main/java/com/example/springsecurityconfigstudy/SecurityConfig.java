690d790d2b17447483f0b3dd87b253238b7dcda3
hjyoo@help-me.kr <hjyoo@help-me.kr>
2022-09-26 20:21:23 +0900

    SecurityContextHolder 전략

--- src/main/java/com/example/springsecurityconfigstudy/SecurityConfig.java
+++ src/main/java/com/example/springsecurityconfigstudy/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @Configuration
 @EnableWebSecurity
@@ -14,24 +16,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .antMatcher("/admin/**")
             .authorizeRequests()
-            .anyRequest().authenticated()
-            .and()
-            .httpBasic();
-    }
-}
-
-@Configuration
-@Order(1)
-class SecurityConfig2 extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+            .anyRequest().authenticated();
         http
-            .authorizeRequests()
-            .anyRequest().permitAll()
-            .and()
-            .formLogin();
+            .formLogin()
+            ;
+        SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
     }
 }
+
--- src/main/java/com/example/springsecurityconfigstudy/SecurityController.java
+++ src/main/java/com/example/springsecurityconfigstudy/SecurityController.java
@@ -1,5 +1,10 @@
 package com.example.springsecurityconfigstudy;
 
+import javax.servlet.http.HttpSession;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,37 +12,28 @@ import org.springframework.web.bind.annotation.RestController;
 public class SecurityController {
 
     @GetMapping("/")
-    public String index() {
+    public String index(HttpSession session) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        SecurityContext context = (SecurityContext) session.getAttribute(
+            HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY);
+        Authentication authentication1 = context.getAuthentication();
+
         return "home";
     }
 
-    @GetMapping("/user")
-    public String user() {
-        return "user";
+    @GetMapping("/thread")
+    public String thread() {
+        new Thread(
+            new Runnable() {
+                @Override
+                public void run() {
+                    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+                }
+            }
+        ).start();
+
+        return "thread";
     }
 
-    @GetMapping("/admin/pay")
-    public String adminPay() {
-        return "adminPay";
-    }
-
-    @GetMapping("/admin/**")
-    public String admin() {
-        return "admin";
-    }
-
-    @GetMapping("/denied")
-    public String denied() {
-        return "Access is denied";
-    }
-
-    @GetMapping("/login")
-    public String login() {
-        return "login";
-    }
-
-    @GetMapping("loginPage")
-    public String loginPage() {
-        return "loginPage";
-    }
 }
